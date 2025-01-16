eaa71e9b3096aa5f32836192d0e6d9363fab85ef
karo-dc <karo.ghazarian@gmail.com>
2025-01-16 13:58:32 +0400

    feat: r2

--- app/src/main/java/com/verifie/android/app/ScanDocumentActivity.java
+++ app/src/main/java/com/verifie/android/app/ScanDocumentActivity.java
@@ -2,7 +2,6 @@ package com.verifie.android.app;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
